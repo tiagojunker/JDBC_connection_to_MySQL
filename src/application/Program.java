f34fd4bfa8df40001fc1de4337a8c5c38fde001e
Tiago Junker <tiago99junker@gmail.com>
2024-07-18 20:39:58 -0300

    SELECT Command by JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,9 +11,30 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeCOnnection();
-
+		Connection conn = null; // Connection to DataBase
+		Statement st = null;	// Object to handle with SQL commands
+		ResultSet rs = null;	// Object to receive the result of Query made by Statement
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM department");  // SQL command (SELECT)
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			// Closing connections
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeCOnnection();
+		}	
+		
+		
 	}
 
 }
