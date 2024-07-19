1c995ccc4872ecfd5cd43b58f0f45aa96c639fca
Tiago Junker <tiago99junker@gmail.com>
2024-07-18 21:21:16 -0300

    INSERT Command with recovery of two or more ID by JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,6 +20,8 @@ public class Program {
 		
 		try {
 			conn = DB.getConnection();
+			
+			/*
 			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
@@ -32,8 +34,14 @@ public class Program {
 			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
+			*/
+			
+			st = conn.prepareStatement(
+					"INSERT INTO department (Name) VALUES ('d1'), ('d2')",
+					Statement.RETURN_GENERATED_KEYS);
 			
 			int rowsAffected = st.executeUpdate();  	// Insert without id recovery
+			
 			if(rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				while(rs.next()) {
@@ -46,8 +54,7 @@ public class Program {
 			
 		} catch(SQLException e) {
 			e.printStackTrace();
-		} catch(ParseException e) {
-			e.printStackTrace();
+			
 		} finally {
 			DB.closeStatement(st);
 			DB.closeCOnnection();
