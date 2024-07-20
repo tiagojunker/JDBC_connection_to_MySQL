26b5b1ba30167747a538140cb252f7406155dd8a
Tiago Junker <tiago99junker@gmail.com>
2024-07-20 11:56:28 -0300

    TRANSACTIONS Command by JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,60 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
+
 		Connection conn = null;
-		PreparedStatement st = null;
-		
-		 try {
-			 conn = DB.getConnection();
-			 
-			 st = conn.prepareStatement(
-					 "DELETE FROM department "
-					 + "WHERE "
-					 + "Id = ?");
-			 
-			 st.setInt(1, 2);
-		 			 
-			 int rowsAffected = st.executeUpdate();
-			 
-			 System.out.println("Done! Rows Affected: " + rowsAffected);
-		 
-		 } catch(SQLException e) {
-			 throw new DbIntegrityException(e.getMessage());
-			 
-		 } finally {
-			 DB.closeStatement(st);
-			 DB.closeCOnnection();
-		 }
-		
+		Statement st = null;
+
+		try {
+			conn = DB.getConnection();
+			conn.setAutoCommit(false);
+			/*
+			 * Open a block to protect the transactions, doing them pending until the code
+			 * makes the confirmation with commit() function.
+			 */
+			st = conn.createStatement();
+
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+			int x = 1;
+			if (x < 2) {
+				throw new SQLException("Fake Error");
+			}
+
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+			// Making the confirmation of transactions
+			conn.commit();
+
+			System.out.println("Rows 1: " + rows1);
+			System.out.println("Rows 2: " + rows2);
+
+		} catch (SQLException e) {
+
+			try {
+
+				// Call The RollBack Function to Undo the changes made.
+				conn.rollback();
+				throw new DbException("Transaction Rolled Back! Coused By: " + e.getMessage());
+
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to Rolled Back! Coused By: " + e1.getMessage());
+			}
+
+		} finally {
+			DB.closeStatement(st);
+			DB.closeCOnnection();
+		}
+
 	}
 
 }
